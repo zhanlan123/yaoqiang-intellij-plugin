3a10b6e0513a55d9a1c2bf535179b150c8bc057f
Blenta <blenta@126.com>
2023-07-06 22:09:52 +0800

    implements LifecycleListener

---
+++ src/main/java/org/yaoqiang/intellij/BPMNEditor.java
@@ -0,0 +1,186 @@
+/**
+ * Yaoqiang BPMN Editor
+ * Copyright (C) 2009-2023 Shi Yaoqiang(blenta@126im.com), All rights reserved.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.yaoqiang.intellij;
+
+import com.intellij.ide.AppLifecycleListener;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxUtils;
+import org.jetbrains.annotations.NotNull;
+import org.yaoqiang.asaf.*;
+import org.yaoqiang.bpmn.editor.actions.BPMNEditorActions;
+import org.yaoqiang.bpmn.editor.menus.ModelMenu;
+import org.yaoqiang.bpmn.editor.utils.BPMNEditorUtils;
+import org.yaoqiang.graph.actions.GraphActions;
+import org.yaoqiang.graph.util.Constants;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+
+public class BPMNEditor {
+
+    static final class LifecycleListener implements AppLifecycleListener {
+        @Override
+        public void appFrameCreated(@NotNull List<String> commandLineArgs) {
+            System.setProperty("asaf.app.name", "plugin");
+            ASAF.setVersion("3.0.0 (Intellij Plugin)");
+
+            String currentLanguage = ASAF.getSetting(ASAF.KEY_LANGUAGE, ASAF.DEFAULT_LANGUAGE);
+            ASAF.setLocale(new Locale(currentLanguage));
+            ResourceMap.add("org.yaoqiang.asaf.resources.locale");
+            ResourceMap.add("org.yaoqiang.graph.resources.graph");
+            ResourceMap.add("org.yaoqiang.bpmn.editor.resources.locale");
+            ASAF.setLogo(ResourceMap.getImageIcon("yaoqiang.Menu.icon").getImage());
+
+            initialize();
+
+            StaticActionMap.addActions(new GraphActions());
+            StaticActionMap.addActions(new BPMNEditorActions());
+
+            initBPMNModelMenu();
+        }
+
+        private void initialize() {
+            File udir = new File(Constants.YAOQIANG_USER_HOME);
+            if (!udir.exists()) {
+                try {
+                    udir.mkdir();
+                } catch (Exception exc) {
+                }
+            }
+
+            File adir = new File(Constants.YAOQIANG_ARTIFACTS_DIR);
+            if (!adir.exists()) {
+                try {
+                    adir.mkdir();
+                } catch (Exception exc) {
+                }
+            }
+
+            File fdir = new File(Constants.YAOQIANG_FRAGMENTS_DIR);
+            if (!fdir.exists()) {
+                try {
+                    fdir.mkdir();
+                } catch (Exception exc) {
+                }
+            }
+
+            Constants.SETTINGS = loadYaoqiangConfigureFile();
+
+            mxConstants.DEFAULT_STARTSIZE = Integer.parseInt(Constants.SETTINGS.getProperty("style_swimlane_title_size", "25"));
+
+            Constants.LAST_FILLCOLOR.clear();
+            String lastFileColor = Constants.SETTINGS.getProperty("lastFillColor", "");
+            List<String> colorList = Arrays.asList(lastFileColor.split(","));
+            int cn = 12;
+            for (String color : colorList) {
+                if (color.length() < 1) {
+                    continue;
+                }
+                cn--;
+                Constants.LAST_FILLCOLOR.add(mxUtils.parseColor(color));
+            }
+            int i = 0;
+            while (cn > 0) {
+                cn--;
+                Constants.LAST_FILLCOLOR.add(Constants.COLORS[i++]);
+            }
+
+            BPMNEditorUtils.setDefaultPageFormat();
+        }
+
+        public void initBPMNModelMenu() {
+            ActionManager actionManager = ActionManager.getInstance();
+            DefaultActionGroup modelMenu = (DefaultActionGroup) actionManager.getAction("yaoqiang.actions.BPMNModelMenu");
+            AppMenu model = new ModelMenu();
+            for (int i = 0; i < model.getItemCount(); i++) {
+                JMenuItem item = model.getItem(i);
+                if (item == null) {
+                    modelMenu.addSeparator();
+                } else {
+                    AnAction action = new AnAction(item.getText()) {
+                        @Override
+                        public void actionPerformed(AnActionEvent e) {
+                            item.getAction().actionPerformed(new ActionEvent(e, 0, ""));
+                        }
+                    };
+                    modelMenu.add(action);
+                }
+            }
+        }
+
+        public  void appClosing() {
+            String colorString = "";
+            for (Color c : Constants.LAST_FILLCOLOR) {
+                colorString += mxUtils.hexString(c) + ",";
+            }
+            Constants.SETTINGS.setProperty("lastFillColor", colorString);
+
+            Utils.saveASAFProperties(ASAF.getSettings());
+            saveYaoqiangConfigureFile();
+        }
+    }
+
+    private static Properties loadYaoqiangConfigureFile() {
+        File file = new File(Constants.YAOQIANG_CONF_FILE);
+
+        Properties props = new Properties();
+        if (file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                props.load(fis);
+                fis.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return props;
+    }
+
+    private static void saveYaoqiangConfigureFile() {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(new File(Constants.YAOQIANG_CONF_FILE));
+            Constants.SETTINGS.store(out, "Yaoqiang BPMN Editor Configuration");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
--- src/main/java/org/yaoqiang/intellij/editor/BPMNFileEditor.java
+++ src/main/java/org/yaoqiang/intellij/editor/BPMNFileEditor.java
@@ -24,7 +24,7 @@ public class BPMNFileEditor extends UserDataHolderBase implements FileEditor {
     }
 
     @Override
-    public @NotNull JComponent getComponent() {
+    public @NotNull BPMNEditorDiagramTab getComponent() {
         return editor;
     }
 
